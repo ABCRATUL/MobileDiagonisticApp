241c2dc45968562187322f8eda0a19a11e0ddf87
pronoymukherjee <mukherjee_pronoy@yaho.in>
2018-06-18 18:31:30 +0530

    Added the Layouts for Test Status and the dialog.

--- app/src/main/java/com/example/pronoymukherjee/hyperxchangediagnosticnew/Activities/TestStatusActivity.java
+++ app/src/main/java/com/example/pronoymukherjee/hyperxchangediagnosticnew/Activities/TestStatusActivity.java
@@ -1,18 +1,15 @@
 package com.example.pronoymukherjee.hyperxchangediagnosticnew.Activities;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pronoymukherjee.hyperxchangediagnosticnew.R;
 
-public class StartTestScreen extends AppCompatActivity {
+public class TestStatusActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_test_screen);
-        /*Intent intent=new Intent(StartTestScreen.this,AutoTestScreen.class);
-        startActivity(intent);*/
+        setContentView(R.layout.activity_test_status);
     }
 }
