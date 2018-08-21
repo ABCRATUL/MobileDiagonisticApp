b2595704cd47c352ea5057a322fa3dfb64967c7a
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-08-21 15:38:05 +0530

    Added the Passcode code UI.

--- app/src/main/java/com/hyperxchange/pronoymukherjee/hyperxchangediagnosticnew/Activities/PinCheckActivity.java
+++ app/src/main/java/com/hyperxchange/pronoymukherjee/hyperxchangediagnosticnew/Activities/PinCheckActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.hyperxchange.pronoymukherjee.hyperxchangediagnosticnew.R;
 
 public class PinCheckActivity extends AppCompatActivity {
-    AppCompatEditText _pinNumber;
+    AppCompatEditText _pin1, _pin2, _pin3, _pin4;
     AppCompatButton _loginButton, _exitButton;
 
     @Override
@@ -18,10 +20,29 @@ public class PinCheckActivity extends AppCompatActivity {
         setTitle("");
         this.setFinishOnTouchOutside(false);
         initializeViews();
+        _pin1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void initializeViews() {
-        _pinNumber = findViewById(R.id.pinEnter);
+        _pin1 = findViewById(R.id.pin1);
+        _pin2 = findViewById(R.id.pin2);
+        _pin3 = findViewById(R.id.pin3);
+        _pin4 = findViewById(R.id.pin4);
         _loginButton = findViewById(R.id.submitPin);
         _exitButton = findViewById(R.id.exitAppPin);
     }
